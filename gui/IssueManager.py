4f5db4c0478ededbfdfa6c93d165a919b6b69603
Ruchira <ruchiranawarathna@gmail.com>
2018-12-10 12:28:08 +0530

    Calculate issue due date

--- gui/IssueManager.py
+++ gui/IssueManager.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pymysql
+import datetime
 
 button_width = 14
 button_height = 2
@@ -17,15 +18,22 @@ class IssuesWindow:
 
         self.book_id_label = Label(frame, text="Book ID")
         self.student_id_label = Label(frame, text="Student ID")
+        self.from_date_label = Label(frame, text="From Date")
 
         self.book_id_field = Entry(frame)
         self.student_id_field = Entry(frame)
+        self.from_date_field = Entry(frame)
 
         self.book_id_label.grid(row=0, sticky=E)
         self.student_id_label.grid(row=1, sticky=E)
+        self.from_date_label.grid(row=2, sticky=E)
         self.book_id_field.grid(row=0, column=1)
         self.student_id_field.grid(row=1, column=1)
+        self.from_date_field.grid(row=2, column=1)
 
+        self.from_date = datetime.datetime.today().strftime('%Y-%m-%d')
+        self.from_date_field.delete(0, 'end')
+        self.from_date_field.insert(0, self.from_date)
         self.add_button = Button(bottom_frame, width=button_width, height=button_height, fg="green", text="ADD", command=self.add)
         self.add_button.config(relief=RAISED)
         self.add_button.pack(side=LEFT)
@@ -36,10 +44,12 @@ class IssuesWindow:
         student_id=self.student_id_field.get()
         print(book_id, student_id)
 
+        due_date = datetime.datetime.strptime(self.from_date, '%Y-%m-%d') + datetime.timedelta(days=14)
+        print(due_date.date())
         conn = pymysql.connect(host='localhost', user='root', password='root', db='library')
         cursor = conn.cursor()
-        add = "INSERT INTO `issues` (`book_id`, `student_id`) VALUES (%s, %s)"
-        val = (book_id, student_id)
+        add = "INSERT INTO `issues` (`book_id`, `student_id`, `from_date`, `due_date`) VALUES (%s, %s, %s, %s)"
+        val = (book_id, student_id, self.from_date, due_date.date())
         cursor.execute(add, val)
         conn.commit()
 
@@ -113,11 +123,17 @@ class ShowIssuesWindow:
         Label(table, text="Issue_ID", bg=bg_color, relief=GROOVE).grid(row=0, column=0, sticky="nsew")
         Label(table, text="Book_ID", bg=bg_color, relief=GROOVE).grid(row=0, column=1, sticky="nsew")
         Label(table, text="Student_ID", bg=bg_color, relief=GROOVE).grid(row=0, column=2, sticky="nsew")
+        Label(table, text="From_Date", bg=bg_color, relief=GROOVE).grid(row=0, column=3, sticky="nsew")
+        Label(table, text="Due_Date", bg=bg_color, relief=GROOVE).grid(row=0, column=4, sticky="nsew")
+        Label(table, text="Fine", bg=bg_color, relief=GROOVE).grid(row=0, column=5, sticky="nsew")
 
         for index, dat in enumerate(data):
             Label(table, text=dat[0], relief=GROOVE).grid(row=index + 1, column=0, sticky="nsew")
             Label(table, text=dat[1], relief=GROOVE).grid(row=index + 1, column=1, sticky="nsew")
             Label(table, text=dat[2], relief=GROOVE).grid(row=index + 1, column=2, sticky="nsew")
+            Label(table, text=dat[3], relief=GROOVE).grid(row=index + 1, column=3, sticky="nsew")
+            Label(table, text=dat[4], relief=GROOVE).grid(row=index + 1, column=4, sticky="nsew")
+            Label(table, text=dat[5], relief=GROOVE).grid(row=index + 1, column=5, sticky="nsew")
 
         table.mainloop()
 # root = Tk()
